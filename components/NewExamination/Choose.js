345a7e8dfb29ad7652026677e6b7e2eee6346916
Karol Bartyzel <karolbartyzel308@gmail.com>
2019-05-28 23:13:40 +0200

    Finish flow of new examination.

--- components/NewExamination/Choose.js
+++ components/NewExamination/Choose.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, StyleSheet, FlatList, TouchableHighlight } from 'react-native';
-import { Button, TextInput, Text, Card, ActivityIndicator } from 'react-native-paper';
+import { View, StyleSheet, } from 'react-native';
+import { Button, TextInput, Card, ActivityIndicator } from 'react-native-paper';
+import { REACT_APP_SERVER_URL, } from 'react-native-dotenv';
 import PropTypes from 'prop-types';
 
 class Choose extends React.Component {
@@ -13,6 +14,31 @@ class Choose extends React.Component {
 
     onPress = (testId) => {
         this.props.setActiveTest(testId);
+    }
+
+    onFinish = () => {
+        const date = Date.now();
+        const data = JSON.stringify({
+            patientId: this.props.patientId,
+            date,
+            examinationsResults: this.props.tests.filter(test => test.filledQuestions.length === test.questions.length).map((test) => {
+                return {
+                    title: test.name,
+                    score: test.score,
+                    maxScore: test.maxScore,
+                    description: test.description,
+                };
+            })
+        });
+        fetch(`${REACT_APP_SERVER_URL}/examination`, {
+            method: 'POST',
+            headers: { Accept: 'application/json', 'Content-Type': 'application/json', },
+            body: data
+        })
+        .then(async (response) => {
+            const newExamination = (await response.json());
+            this.props.finishTest(newExamination._id, newExamination.date);
+        });
     }
 
     render() {
@@ -40,13 +66,24 @@ class Choose extends React.Component {
                                 contentStyle={styles.listEntryText}
                                 onPress={() => this.onPress(test.id)}
                                 dark={true}
-                                disabled={this.props.testsResults.find(testResult => testResult.testId === test.id)}
+                                color={test.filledQuestions.length === test.questions.length ? 'green' : test.filledQuestions.length > 0 ? 'orange' : 'brown'}
                                 uppercase={false}
                             >
-                                {test.shortname}
+                                {test.title} {test.filledQuestions.length} / {test.questions.length}
                             </Button>
                         ))}
                     </Card.Content>
+                    <Card.Actions>
+                        <Button
+                            mode="contained"
+                            style={styles.finishButton}
+                            onPress={this.onFinish}
+                            dark={true}
+                            disabled={this.props.tests.filter(test => test.filledQuestions.length === test.questions.length) === 0 || !this.props.patientId}
+                        >
+                            Zakończ badanie
+                        </Button>
+                    </Card.Actions>
                 </Card>
             </View>
         );
@@ -59,7 +96,7 @@ const styles = StyleSheet.create({
     },
     input: {
         margin: 10,
-        marginTop: -10,
+        marginTop: 10,
     },
     listContent: {
         flex: 1,
@@ -73,6 +110,11 @@ const styles = StyleSheet.create({
         paddingBottom: 5,
         display: 'flex',
         justifyContent: 'flex-start'
+    },
+    finishButton: {
+        width: '100%',
+        paddingTop: 5,
+        paddingBottom: 5,
     }
 });
 
